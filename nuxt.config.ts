aa9f1f14280312ab429f7174a2b4305799a809ca
Benediktus Hosni I <37971110+Reivu@users.noreply.github.com>
2024-02-18 20:35:48 +0700

    Update
    .editorconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  app: {
+    head: {
+      title: 'Nuxt Blog',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'learn nuxt' }
+      ]
+    }
+  },
+  typescript: {
+    shim: false
+  },
   tailwindcss: {},
 })
